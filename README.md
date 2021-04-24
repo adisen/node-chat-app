5625c46ccbbb9fdb9cbfea064dd8f9b35880f300
Israel Adetunji <israeleadetunji@gmail.com>
2021-04-24 14:47:22 +0100

    Added Snowplow analytics tracking

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Node Chat App
 
 This is a chat app implemented with NodeJS and SocketIO
+
+## Setup and Installtion
+
+1. Clone the repo with:
+
+```bash
+~user: $ git clone git@github.com:adisen/node-chat-app.git
+```
+
+2. Install Dependencies
+
+```bash
+~user: $ cd node-chat-app
+~user: $ npm i
+```
+
+3. Start the app
+
+```bash
+~user: $ npm run server
+```
+
+## Using the App
+
+This app is a full-stack app that allows users to chat in a group. There are two pages.
+
+1. The user enters the user name they want to use in the group and also the name of the group. This can be any name. The user just needs to make sure that they enter the same name as the name of the group where the other people they want to chat with are.  
+   **N.B: Group names are case sensitive.**
+
+2. The second page is where the user chats on the group. The list of users on the group is shown on the left side. A user can send a text message and they can also send their location.
+
+_App hosted [Here](https://nameless-peak-41750.herokuapp.com/)_
--- server/server.js
+++ server/server.js
@@ -1,88 +1,89 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const express = require('express')
+const app = express()
 
-const { generateMessage, generateLocationMessage } = require("./utils/message");
-const { isRealString } = require("./utils/validation");
-const { Users } = require("./utils/users");
+const path = require('path')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const cors = require('cors')
 
-const publicPath = path.join(__dirname, "../public");
-app.use(express.static(publicPath));
+app.use(cors())
 
-let users = new Users();
+const { generateMessage, generateLocationMessage } = require('./utils/message')
+const { isRealString } = require('./utils/validation')
+const { Users } = require('./utils/users')
 
-io.on("connection", socket => {
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath))
+
+let users = new Users()
+
+io.on('connection', socket => {
   // console.log("a user connected");
 
   //On user join
-  socket.on("join", (params, callback) => {
+  socket.on('join', (params, callback) => {
     if (!isRealString(params.name) && !isRealString(params.room)) {
-      return callback("Name and room name are required");
+      return callback('Name and room name are required')
     }
 
-    socket.join(params.room);
-    users.removeUser(socket.id);
-    users.addUser(socket.id, params.name, params.room);
+    socket.join(params.room)
+    users.removeUser(socket.id)
+    users.addUser(socket.id, params.name, params.room)
 
-    io.to(params.room).emit("updateUserList", users.getUserList(params.room));
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room))
 
     socket.emit(
-      "newMessage",
-      generateMessage("Admin", "Welcome to the chat app")
-    );
+      'newMessage',
+      generateMessage('Admin', 'Welcome to the chat app')
+    )
 
     socket.broadcast
       .to(params.room)
-      .emit(
-        "newMessage",
-        generateMessage("Admin", `${params.name} has joined`)
-      );
+      .emit('newMessage', generateMessage('Admin', `${params.name} has joined`))
 
-    callback();
-  });
+    callback()
+  })
 
   // On user send new message
-  socket.on("createMessage", (message, callback) => {
-    let user = users.getUser(socket.id);
+  socket.on('createMessage', (message, callback) => {
+    let user = users.getUser(socket.id)
 
     if (user && isRealString(message.text)) {
       io.to(user.room).emit(
-        "newMessage",
+        'newMessage',
         generateMessage(user.name, message.text)
-      );
-      callback();
+      )
+      callback()
     }
-  });
+  })
 
   // On user send location
-  socket.on("createLocationMessage", coords => {
-    let user = users.getUser(socket.id);
+  socket.on('createLocationMessage', coords => {
+    let user = users.getUser(socket.id)
 
     if (user) {
       io.to(user.room).emit(
-        "newLocationMessage",
+        'newLocationMessage',
         generateLocationMessage(user.name, coords.latitude, coords.longitude)
-      );
+      )
     }
-  });
+  })
 
   // On user disconnect
-  socket.on("disconnect", () => {
-    let user = users.removeUser(socket.id);
+  socket.on('disconnect', () => {
+    let user = users.removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room))
       io.to(user.room).emit(
-        "newMessage",
-        generateMessage("Admin", `${user.name} has left`)
-      );
+        'newMessage',
+        generateMessage('Admin', `${user.name} has left`)
+      )
     }
-  });
-});
+  })
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 http.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
-});
+  console.log(`Server started on port: ${PORT}`)
+})
